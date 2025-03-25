7cfee4975bdc01517244dda851a5d11e17dcc5cc
msino88 <18142802+msino88@users.noreply.github.com>
2025-03-25 01:14:48 -0400

    changes to css and home.js to include bolded text and other stuff

--- einelise-react/src/components/Home.js
+++ einelise-react/src/components/Home.js
@@ -10,26 +10,28 @@ export default function Home() {
                 <h2>Stats:</h2>
                 <p>As of 3/24/2025, Einelise's stats are:</p>
                 <ul>
-                    <li>Breed: Pembroke Welsh Corgi</li>
-                    <li>Weight: 11 lbs</li>
-                    <li>Age: 18 weeks old</li>
-                    <li>Favorite Treat: Chicken</li>
-                    <li>Cuteness: 100/10</li>
+                    <li><strong>Breed</strong>: Pembroke Welsh Corgi</li>
+                    <li><strong>Birthday</strong>: November 6th, 2024</li>
+                    <li><strong>Weight</strong>: ~11 lbs</li>
+                    <li><strong>Age</strong>: 18 weeks old</li>
+                    <li><strong>Favorite Treat</strong>: Chicken</li>
+                    <li><strong>Cuteness</strong>: 100/10</li>
                 </ul>
 
             </div>
             <div className="panel-tricks">
+                <h2>Tricks:</h2>
                 <p>Knows the following tricks:</p>
                 <ul>
-                    <li>Sit</li>
-                    <li>Down</li>
-                    <li>Up</li>
-                    <li>Crate</li>
-                    <li>Left/Right Paw</li>
-                    <li>Left/Right Twirl</li>
-                    <li>Stand</li>
-                    <li>Give me a kiss!</li>
-                    <li>Cuddles</li>
+                    <li><strong>Sit</strong> - Einelise sits!</li>
+                    <li><strong>Down</strong> - Einelise goes into a down (we're trying to eventually teach her to sploot)</li>
+                    <li><strong>Up</strong> - Einelise will get on her hind legs and stand up, jumping all the while!</li>
+                    <li><strong>Crate</strong> - She knows how to enter her crate on command</li>
+                    <li><strong>Left/Right Paw</strong> - Einelise will put her Left/Right paw out depending on which one you ask for (50/50) chance</li>
+                    <li><strong>Left/Right Twirl</strong> - Einelise will do a Left/Right twirl dependng on which one you ask for. So far, she needs visual assistance to figure out which way to twirl.</li>
+                    <li><strong>Stand</strong> - Einelise stands from a down/sit position</li>
+                    <li><strong>Give me a kiss!</strong> - Einelise will bump her snoot in your general facial area, mimicking a kiss!</li>
+                    <li><strong>Cuddles</strong> - Einelise will crawl into your lap and cuddle with you</li>
                 </ul>
             </div>
         </div>
