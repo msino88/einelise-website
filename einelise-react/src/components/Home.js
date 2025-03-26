1c6e6a216960017d175ded1ddab9d1014a7e242f
msino88 <18142802+msino88@users.noreply.github.com>
2025-03-25 23:12:47 -0400

    adds corgi background to public images folder; edits to styles.css to include border; changes to home.js

--- einelise-react/src/components/Home.js
+++ einelise-react/src/components/Home.js
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div className="home-container">
-            <div className="panel bio">
+            <div className="panel-bio">
             <h1> Welcome to Einelise's Page!</h1>
             <p>Hello, welcome to Einelise's Webpage. Here you will find all things Einelise-related, including her Bio, stats, and more!</p>
             </div>
